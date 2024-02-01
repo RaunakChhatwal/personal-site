b8fe2c40630769297130930887e36ccfab3f3ba3
Raunak Chhatwal <raunakchhatwal001@gmail.com>
2024-02-01 03:58:09 -0500

    ported header.rs to tailwind

--- client/src/header.rs
+++ client/src/header.rs
@@ -13,7 +13,8 @@ fn HeaderLink(text_content: &'static str, href: &'static str, new_tab: bool) -> 
 
     create_effect(move |_| {
         if location.pathname.get() == *href || hover.get() {
-            set_background_color.set("rgb(32, 35, 37)")
+            let hover_color = "rgb(32, 35, 37)";
+            set_background_color.set(hover_color)
         } else {
             set_background_color.set(BACKGROUND_COLOR)
         }
@@ -23,13 +24,9 @@ fn HeaderLink(text_content: &'static str, href: &'static str, new_tab: bool) -> 
         <a href={href}
             on:mouseenter=move |_| set_hover.set(true)
             on:mouseleave=move |_| set_hover.set(false)
+            class="m-0 text-center block no-underline text-inherit"
             style:background-color=background_color
-            style:margin="0"
             style:width="7vw"
-            style:text-align="center"
-            style:display="block"
-            style:text-decoration="none"
-            style:color="inherit"
             style:font="inherit"
             style:line-height=HEADER_HEIGHT
         >{text_content}</a>
@@ -47,29 +44,21 @@ fn HeaderLink(text_content: &'static str, href: &'static str, new_tab: bool) -> 
 pub fn Header() -> impl IntoView {
     view! {
         <header
+            class="flex"
             style:height=HEADER_HEIGHT
             style:font-size="1.1em"
-            style:display="flex"
         >
             <nav>
-                <div
-                    style:display="flex"
-                    style:justify-content="flex-start"
-                >
+                <div class="flex justify-start">
                     <HeaderLink text_content="About" href="/" new_tab=false />
                     <HeaderLink text_content="Resume" href="/resume" new_tab=false />
                     <HeaderLink text_content="Projects" href="/projects" new_tab=false />
                     <HeaderLink text_content="Interests" href="/interests" new_tab=false />
                 </div>
             </nav>
-            <div
-                style:display="flex"
-                style:justify-content="flex-end"
-                style:align-items="center"
-                style:width="100%"
-            >
-                <p style:margin="0 1em" style:text-align="center" style:line-height=HEADER_HEIGHT>"raunakchhatwal001@gmail.com"</p>
-                <div style:width="1px" style:height="1em" style:margin=0 style:background-color=TEXT_COLOR></div>
+            <div class="flex justify-end items-center w-full">
+                <p class="text-center" style:margin="0 1em" style:line-height=HEADER_HEIGHT>"raunakchhatwal001@gmail.com"</p>
+                <div class="m-0" style:width="1px" style:height="1em" style:background-color=TEXT_COLOR></div>
                 <HeaderLink text_content="LinkedIn" href=LINKEDIN_URL new_tab=true />
                 <HeaderLink text_content="Github" href="https://github.com/RaunakChhatwal" new_tab=true />
             </div>
